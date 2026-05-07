a00b9afd2132416ea873fc3257a983746ee50993
DucNguyen <ducnguyenvh9009@gmail.com>
2026-05-07 07:29:34 +0700

    deploy

--- data/SQLQuery1.sql
+++ data/SQLQuery1.sql
@@ -51,6 +51,49 @@ INSERT INTO students (name, email) VALUES
 
 (N'Phùng Thị Zui', 'zui.phung20@gmail.com');
 
+
+INSERT INTO students (name, email) VALUES
+
+('Nguyen Van An', 'an.nguyen01@gmail.com'),
+
+('Tran Thi Binh', 'binh.tran02@gmail.com'),
+
+('Le Van Cuong', 'cuong.le03@gmail.com'),
+
+('Pham Thi Dung', 'dung.pham04@gmail.com'),
+
+('Hoang Van Em', 'em.hoang05@gmail.com'),
+
+('Vu Thi Hanh', 'hanh.vu06@gmail.com'),
+
+('Do Van Khoa', 'khoa.do07@gmail.com'),
+
+('Bui Thi Lan', 'lan.bui08@gmail.com'),
+
+('Ngo Van Minh', 'minh.ngo09@gmail.com'),
+
+('Phan Thi Nga', 'nga.phan10@gmail.com'),
+
+('Dang Van Phuc', 'phuc.dang11@gmail.com'),
+
+('Ly Thi Quynh', 'quynh.ly12@gmail.com'),
+
+('Ta Van Son', 'son.ta13@gmail.com'),
+
+('Chu Thi Trang', 'trang.chu14@gmail.com'),
+
+('Dinh Van Tuan', 'tuan.dinh15@gmail.com'),
+
+('Cao Thi Uyen', 'uyen.cao16@gmail.com'),
+
+('Luong Van Vinh', 'vinh.luong17@gmail.com'),
+
+('Ha Thi Xuan', 'xuan.ha18@gmail.com'),
+
+('Vo Van Yen', 'yen.vo19@gmail.com'),
+
+('Phung Thi Zui', 'zui.phung20@gmail.com');
+
 DROP TABLE students;
 SELECT * FROM students;
 EXEC sp_help students;
